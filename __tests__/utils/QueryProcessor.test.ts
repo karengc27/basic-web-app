b53a624472ded4b76e9eabba56a710c9c5796d95
Karen Gonzalez <kareng@andrew.cmu.edu>
2024-10-10 10:09:21 -0400

    Added find largest and add

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -25,4 +25,9 @@ describe("QueryProcessor", () => {
             "we can't give u that info lol"
           ));
     });
+
+    test('should add numbers', () => {
+        expect(QueryProcessor("What is 5 plus 52?")).toEqual("57");
+        expect(QueryProcessor("What is 12 plus 1?")).toEqual("13");
+    });
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -19,5 +19,21 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  const addMatch = query.match(/What is (\d+) plus (\d+)/);
+  if (addMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
+  }
+  // Which of the following numbers is the largest: 41, 93, 11?
+  const findLargest = query.match(/Which of the following numbers is the largest: (\d+), (\d+), (\d+)/);
+  if (findLargest) {
+    const x: number = parseInt(findLargest[1]);
+    const y: number = parseInt(findLargest[2]);
+    const z: number = parseInt(findLargest[2]);
+
+    return Math.max(x, y, z).toString();
+  }
+
   return "";
 }
